97bc953bbe412b7aecaa00500c1bbd3176cfb729
Nils Werner <nils.werner@gmail.com>
2012-09-24 11:29:12 +0200

    Add more paramters to the script, display help if needed. Remove non-verbosity

--- tools/build.sh
+++ tools/build.sh
@@ -2,11 +2,18 @@
 
 DOZIP=false
 DODELETE=false
+HASVERSION=false
 
-while getopts "v:zd" opt; do
+WORSPACEVERSION="integration"
+
+bold=`tput bold`
+normal=`tput sgr0`
+
+while getopts "v:w:zdh" opt; do
 	case $opt in
 		v)
 			VERSION=$OPTARG
+			HASVERSION=true
 		;;
 		z)
 			DOZIP=true
@@ -14,33 +21,56 @@ while getopts "v:zd" opt; do
 		d)
 			DODELETE=true
 		;;
+		w)
+			WORSPACEVERSION=$OPTARG
+		;;
+		h)
+			echo "Symphony builder options"
+			echo ""
+			echo " ${bold}-v${normal} version"
+			echo "    Define Symphony version to be fetched. Must be a valid Git ref."
+			echo " ${bold}-w${normal} version"
+			echo "    Define workspace version to be fetched. Must be a valid Git ref, defaults to integration."
+			echo " ${bold}-z${normal}"
+			echo "    Create zip file."
+			echo " ${bold}-d${normal}"
+			echo "    Delete fetched data after finishing all jobs."
+			echo " ${bold}-h${normal}"
+			echo "    This help."
+			exit 0
+		;;
 		\?)
-			echo "Invalid option: -$OPTARG" >&2
+			echo "Invalid option: ${bold}-$OPTARG${normal}" >&2
 			exit 1
 		;;
 		:)
-			echo "Option -$OPTARG requires an argument." >&2
+			echo "Option ${bold}-$OPTARG${normal} requires an argument." >&2
 			exit 1
 		;;
 	esac
 done
 
+if [ $HASVERSION == false ]; then
+	echo "Option ${bold}-v${normal} is required. See ${bold}-h${normal} for all options." >&2
+	exit 1
+fi
+
 echo Clone repository
-git clone git://github.com/symphonycms/symphony-2.git symphony-$VERSION &> /dev/null
+git clone git://github.com/symphonycms/symphony-2.git symphony-$VERSION
 
 echo Checkout desired version $VERSION
 cd symphony-$VERSION
-git checkout $VERSION &> /dev/null
+git checkout $VERSION
 
 echo Initialize submodules
-git submodule update --init &> /dev/null
+git submodule update --init
 
 echo Clone Workspace
-git clone git://github.com/symphonycms/workspace.git &> /dev/null
+git clone git://github.com/symphonycms/workspace.git
 
 echo Checkout latest version of workspace
 cd workspace
-git checkout integration &> /dev/null
+git checkout $WORSPACEVERSION
 cd ../
 
 echo Remove Git cruft
@@ -52,7 +82,7 @@ cd ../
 
 if [ $DOZIP == true ]; then
 	echo Zip it
-	zip -r symphony$VERSION.zip symphony-$VERSION > /dev/null
+	zip -r symphony$VERSION.zip symphony-$VERSION
 fi
 
 if [ $DODELETE == true ]; then
