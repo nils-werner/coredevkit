15137c55a3fa950354bdc3c4c3d4d0d79a06cf2f
Nils Werner <nils.werner@gmail.com>
2012-10-23 13:46:13 +0200

    Enable Errors when extension is enabled

--- extension.driver.php
+++ extension.driver.php
@@ -6,7 +6,20 @@
 	-------------------------------------------------------------------------*/
 
 		public function getSubscribedDelegates() {
+			return array(
+						array(
+							'page'		=> '/backend/',
+							'delegate'	=> 'InitaliseAdminPageHead',
+							'callback'	=> 'initaliseAdminPageHead'
+						),
+					);
 		}
+
+		public function initaliseAdminPageHead($context) {
+			error_reporting(E_ALL);
+			ini_set('display_errors', 1);
+		}
+
 	}
 
 ?>
